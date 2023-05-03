ca729d22c1b6a26cdc5a9492505af421175157a7
WangJianping996 <61378716+WangJianping996@users.noreply.github.com>
2023-05-03 16:48:31 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -6,9 +6,7 @@
 
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
+    "https://www.maimemo.com/share/page?uid=2338953&pid=68de9a55f1e706b9b6cffaf925a502a1&tid=fd91a19dd048c2acc3e65667654d619f",
   ]
 =====================以上为范例！=====================
 
